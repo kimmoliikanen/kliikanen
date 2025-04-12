46ee4cb4af8c13e1a27f306bf7a36cf256205fec
kimmo.liikanen@myy.haaga-helia.fi <kimmo.liikanen@myy.haaga-helia.fi>
2025-04-12 23:45:08 +0300

    npm build up

--- viikko3/src/App.tsx
+++ viikko3/src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect, useCallback } from 'react'
-import reactLogo from '/react.svg'
+import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
