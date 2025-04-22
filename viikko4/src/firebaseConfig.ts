2ac6c710d22b4cf3a2d8ca029a8719a91840ef81
kimmo.liikanen@myy.haaga-helia.fi <kimmo.liikanen@myy.haaga-helia.fi>
2025-04-22 20:41:32 +0300

    firebase-autentikointi

---
+++ viikko4/src/firebaseConfig.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBLWQeq6o_eUwVlU4L1rivAKbYMQXk7lns",
+    authDomain: "pilvi-vite-9db75.firebaseapp.com",
+    projectId: "pilvi-vite-9db75",
+    sstorageBucket: "pilvi-vite-9db75.firebasestorage.app",
+    messagingSenderId: "771775700006",
+    appId: "1:771775700006:web:1d1336cd8bb07d74669ae7"
+  };
+
+export default firebaseConfig;
